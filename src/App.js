f337175622a3fd7dfbfd3eddc8b012b3424efcbb
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-04-09 21:25:18 -0300

    Criando rotas para pagina login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import Home from "./pages/Home";
-
 import { ThemeProvider } from '@mui/material/styles';
+
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
 
 import theme from "./theme";
 
 function App() {
+  console.log(window.location.href);
+  const url = window.location.href; 
 
   return (
-
-    <ThemeProvider theme={theme}>
-        <Home/>
-    </ThemeProvider>
-
     
+    <ThemeProvider theme={theme}>
+      {
+      url ===  'http://localhost:3000/'
+      ? <Home />
+
+      : <SignIn />
+      }
+
+       
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/Signin/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SignIn() {
+    return(
+        <h1>Login componente</h1>
+    );
+}
+
+export default SignIn;
