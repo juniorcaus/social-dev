b98dbd4bc6043408d9ec4ea9ff0211dec17ec3b6
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-04-10 16:49:34 -0300

    Criando rotas usando Router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import SignIn from "./pages/Signin";
@@ -7,18 +8,24 @@ import SignIn from "./pages/Signin";
 import theme from "./theme";
 
 function App() {
-  console.log(window.location.href);
-  const url = window.location.href; 
 
   return (
     
     <ThemeProvider theme={theme}>
-      {
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/sign-in" element={ <SignIn /> } />
+          <Route path="*" element={ <h1>Not Found - 404</h1> } />
+        </Routes>
+      
+      </BrowserRouter>
+     {/*  {
       url ===  'http://localhost:3000/'
       ? <Home />
 
       : <SignIn />
-      }
+      } */}
 
        
     </ThemeProvider>
