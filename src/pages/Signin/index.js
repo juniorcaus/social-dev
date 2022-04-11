9cf9ab2301a3d2849bb55521e109f5707f155c12
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-04-10 23:12:00 -0300

    customizando componentes de login

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,8 +1,40 @@
 import React from "react";
+import './style.css';
+
+import { makeStyles } from '@material-ui/styles';
+import typography from "../../theme/typography";
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'grow',
+        height: '100vh',
+    }
+})
 
 function SignIn() {
+    const classes = useStyles();
+
     return(
-        <h1>Login componente</h1>
+        <div className={classes.root}>{/*  FLEX CONTAINER */}
+        
+            <div className="left">{/*  FLEX ITEM CONTAINER */}
+                <h2 className="titulo">Simplificando a forma de conectar desenvolvedores</h2>
+                    
+                <h4 className="subtitulo">Compartilhe seu conhecimento com toda nossa rede de desenvolvedores</h4>
+            </div>
+
+            <div className="right">{/* FLEX ITEM */}
+                <form className="form">
+                    <h4>ACESSO</h4>
+                    <input type="text" />
+                    <input type="text" />
+
+                </form>
+                
+            </div>    
+
+        </div>
     );
 }
 
